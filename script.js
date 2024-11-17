ef3b961bd208f185eb20885181adb58f0accf9d8
AdinaYounus <adinayounusm@gmail.com>
2024-11-17 00:56:09 -0500

    big changes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-    x.className += " responsive";
-    } else {
-    x.className = "topnav";
-    }
-}
+// const hamburger = document.getElementById('hamburger')
+
+// hamburger.addEventListener("click", toggleMenu());
+// function toggleMenu() {
+//     hamburger.addEventListener("click");
+//     hamburger.classList.toggle('open');
+// }
+
+
 
 //taking data and putting to json form allowing to read the data 
 const response = await fetch('products.json');
@@ -19,12 +20,22 @@ const prodCard = vinyl => {
     let cardGrid = document.createElement("div");
     cardGrid.classList.add("cardGrid");
     cardGrid.innerHTML = `
-        <h1>${vinyl.title}</h1>
+        <img src="${vinyl.cover}" alt="vinyl cover">
+        <h2>${vinyl.title}</h2>
+        <h5>${vinyl.artist}</h5>
+        <p>${vinyl.price}</p>
+        <button>Add to Cart</button>
+        
     `
     return cardGrid; 
 }
 
-json.products.forEach((vinyl) => {
-    let card = prodCard(vinyl);
+// json.products.forEach((vinyl) => {
+//     let card = prodCard(vinyl);
+//     productGrid.appendChild(card);
+// })
+
+for (let i = 0; i<4; i++){
+    let card = prodCard(json.products[i]);
     productGrid.appendChild(card);
-})
+}
